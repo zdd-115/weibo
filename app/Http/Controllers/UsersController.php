936ad140ad263a705c396981f63b37849046bdfc
zdd-115 <zhangxd@htemp.com>
2020-09-23 09:24:29 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,18 @@ use Auth;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -39,13 +51,15 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
-            'name'     => 'required|max:50',
+            'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
 
@@ -56,8 +70,8 @@ class UsersController extends Controller
         }
         $user->update($data);
 
-        session()->flash('success', "个人资料更新成功！");
+        session()->flash('success', '个人资料更新成功！');
 
-        return redirect()->route('users.show', $user);
+        return redirect()->route('users.show', $user->id);
     }
 }
