7bde365c9b606011df0d0a01cb6785eecf105992
zdd-115 <zhangxd@htemp.com>
2020-09-27 14:56:38 +0800

    用户微博列表

---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Status;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $statuses = factory(Status::class)->times(100)->create();
+    }
+}
